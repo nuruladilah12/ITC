fbb0bfaa800702aaf310d231eb348a9f1a550d9b
nuruladilah12 <100017644+nuruladilah12@users.noreply.github.com>
2022-05-09 00:16:00 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,25 @@
-var nama=prompt('Masukkan nama kamu : ');
-alert('Hai ' + nama + '!');
+const txtElement = ['Mahasiswi', 'Mahasiswi Informatika 2020', 'Mahasiswi di UPN Veteran Yogyakarta'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+      count++;
+      txtIndex = 0;  
+    }
+
+    setTimeout(ngetik, 400);
+
+
+})();
