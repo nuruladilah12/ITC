07abd1388f7e3081eb2c1bb2dafae1aed8df02b0
nuruladilah12 <100017644+nuruladilah12@users.noreply.github.com>
2022-05-09 20:38:13 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,7 @@
-const txtElement = ['Mahasiswi', 'Mahasiswi Informatika 2020', 'Mahasiswi di UPN Veteran Yogyakarta'];
-let count = 0;
-let txtIndex = 0;
-let currentTxt = '';
-let words = '';
-
-(function ngetik(){
-    if(count == txtElement.length){
-        count = 0;
-    }
-
-    currentTxt = txtElement[count];
-
-    words = currentTxt.slice(0, ++txtIndex);
-    document.querySelector('.efek-ngetik').textContent = words;
-
-    if(words.length == currentTxt.length){
-      count++;
-      txtIndex = 0;  
-    }
-
-    setTimeout(ngetik, 400);
-
-
-})();
+<body>
+<script>
+          function myFunction() {
+            alert("Welcome About Us Page!");
+          }
+        </script>
+</body>
